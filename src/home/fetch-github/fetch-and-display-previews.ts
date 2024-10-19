fc09e795426b93ca8b5ce2b2ddf8312f0084d6d7
zugdev <zug.dev@proton.me>
2024-10-19 03:34:37 -0300

    feat: add a filter to display only selected orgs issues/proposals

--- src/home/fetch-github/fetch-and-display-previews.ts
+++ src/home/fetch-github/fetch-and-display-previews.ts
@@ -1,6 +1,7 @@
 import { GitHubIssue } from "../github-types";
 import { taskManager } from "../home";
 import { applyAvatarsToIssues, renderGitHubIssues } from "../rendering/render-github-issues";
+import { selectedOrgs } from "../rendering/render-orgs-selector";
 import { Sorting } from "../sorting/generate-sorting-buttons";
 import { sortIssuesController } from "../sorting/sort-issues-controller";
 import { checkCacheIntegrityAndSyncTasks } from "./cache-integrity";
@@ -35,12 +36,21 @@ function getProposalsOnlyFilter(getProposals: boolean) {
   };
 }
 
+function filterIssuesBySelectedOrgs() {
+  return (issue: GitHubIssue) => {
+    if (!selectedOrgs.size) return true;
+    const orgName = issue.repository_url.split("/").slice(-2, -1)[0];
+    console.log(orgName);
+    return selectedOrgs.has(orgName);
+  };
+}
+
 // checks the cache's integrity, sorts issues, checks Directory/Proposals toggle, renders them and applies avatars
 export async function displayGitHubIssues(sorting?: Sorting, options = { ordering: "normal" }) {
   await checkCacheIntegrityAndSyncTasks();
   const cachedTasks = taskManager.getTasks();
   const sortedIssues = sortIssuesController(cachedTasks, sorting, options);
-  const sortedAndFiltered = sortedIssues.filter(getProposalsOnlyFilter(isProposalOnlyViewer));
+  const sortedAndFiltered = sortedIssues.filter(getProposalsOnlyFilter(isProposalOnlyViewer)).filter(filterIssuesBySelectedOrgs());
   renderGitHubIssues(sortedAndFiltered);
   applyAvatarsToIssues();
 }
